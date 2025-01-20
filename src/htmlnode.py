193750278b834b8db257890c63045f89701c6588
TomtheHoff <hofstetterthomas@gmx.ch>
2025-01-20 20:17:14 +0100

    htmlNode done, work on testing

---
+++ src/htmlnode.py
@@ -0,0 +1,22 @@
+class HTMLNode:
+    def __init__(self, tag = None , value = None, children = None, props = None):
+        self.tag = tag
+        self.value = value
+        self.children = children
+        self.props = props
+
+    def to_html(self):
+        raise NotImplementedError
+    
+    def props_to_html(self):
+        string = ""
+        if self.props == None:
+            pass
+        else:
+            for key, value in self.props.items():
+                string += (f' {key}="{value}"')
+        return string
+    
+    def __repr__(self):
+        return (f"HTMLNode(tag='{self.tag}', value='{self.value}', children={self.children}, props={self.props})")
+
---
+++ src/test_htmlnode.py
@@ -0,0 +1,10 @@
+import unittest
+
+from htmlnode import HTMLNode
+
+class TestHTMLNode(unittest.TestCase):
+    
+
+
+if __name__ == "__main__":
+    unittest.main()
