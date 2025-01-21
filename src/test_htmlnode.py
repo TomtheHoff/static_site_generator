69e9c78d6926ee389dacfcd56c8ebc2baacc02bc
TomtheHoff <hofstetterthomas@gmx.ch>
2025-01-21 20:47:56 +0100

    tests done

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -3,8 +3,21 @@ import unittest
 from htmlnode import HTMLNode
 
 class TestHTMLNode(unittest.TestCase):
-    
+    def test_props_none(self):
+        node = HTMLNode(props=None)
+        self.assertEqual(node.props_to_html(), "")
 
+    def test_props_single(self):
+        node = HTMLNode(props= {"href": "https://www.google.com"})
+        node2 = ' href="https://www.google.com"'
+        self.assertEqual(node.props_to_html(), node2)
+
+    def test_props_multi(self):
+        node = HTMLNode(props= {"href": "https://www.google.com", "target": "_blank"})
+        node2 = ' href="https://www.google.com" target="_blank"'
+        self.assertEqual(node.props_to_html(), node2)    
+
+     
 
 if __name__ == "__main__":
     unittest.main()
