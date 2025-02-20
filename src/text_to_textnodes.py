d6eef3ae5be568eaf537cba839a1b9a5a2e02d9a
TomtheHoff <hofstetterthomas@gmx.ch>
2025-02-20 20:56:25 +0100

    refactor text_to_textnodes function to use list for textnodes and integrate delimiter splitting

--- src/text_to_textnodes.py
+++ src/text_to_textnodes.py
@@ -1,11 +1,24 @@
-from split_nodes import split_nodes_link, split_nodes_image
+from split_nodes import *
 from textnode import TextNode, TextType
 
 
 def text_to_textnodes(text):
-    textnode = ()
-    textnode += text, TextType.TEXT
-    print(f"\033[93m{textnode=}\033[0m")
+    textnode=[]
+    textnode.append(TextNode(text, TextType.TEXT, None))
+    #print(f"\033[93m{textnode=}\033[0m")
+    textnode = split_nodes_image(textnode)
+    #print(f"\033[93m{textnode=}\033[0m")
+    textnode = split_nodes_link(textnode)
+    #print(f"\033[93m{textnode=}\033[0m")
+    textnode = split_nodes_delimiter(textnode, "**", TextType.BOLD)
+    #print(f"\033[93m{textnode=}\033[0m")
+    textnode = split_nodes_delimiter(textnode, "*", TextType.ITALIC)
+    #print(f"\033[93m{textnode=}\033[0m")
+    textnode = split_nodes_delimiter(textnode, "`", TextType.CODE)
+    #print(f"\033[93m{textnode=}\033[0m")
+    return textnode
+
+
 
 
 text = "This is **text** with an *italic* word and a `code block` and an ![obi wan image](https://i.imgur.com/fJRm4Vk.jpeg) and a [link](https://boot.dev)"
