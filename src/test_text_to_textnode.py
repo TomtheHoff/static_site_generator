8285de30596d6c4a831d650e4176dfaa80773081
TomtheHoff <hofstetterthomas@gmx.ch>
2025-02-21 15:09:13 +0100

    add unit tests for text_to_textnodes function

---
+++ src/test_text_to_textnode.py
@@ -0,0 +1,31 @@
+import unittest
+
+from text_to_textnodes import text_to_textnodes
+from textnode import TextNode, TextType
+
+class TestTextToTextNodes(unittest.TestCase):
+    def test_normal(self):
+        node = "This is **text** with an *italic* word and a `code block` and an ![obi wan image](https://i.imgur.com/fJRm4Vk.jpeg) and a [link](https://boot.dev)"
+        expected_nodes = [
+    TextNode("This is ", TextType.TEXT),
+    TextNode("text", TextType.BOLD),
+    TextNode(" with an ", TextType.TEXT),
+    TextNode("italic", TextType.ITALIC),
+    TextNode(" word and a ", TextType.TEXT),
+    TextNode("code block", TextType.CODE),
+    TextNode(" and an ", TextType.TEXT),
+    TextNode("obi wan image", TextType.IMAGE, "https://i.imgur.com/fJRm4Vk.jpeg"),
+    TextNode(" and a ", TextType.TEXT),
+    TextNode("link", TextType.LINK, "https://boot.dev"),
+]
+        result = text_to_textnodes(node)
+        self.assertEqual(result, expected_nodes)
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    unittest.main()
